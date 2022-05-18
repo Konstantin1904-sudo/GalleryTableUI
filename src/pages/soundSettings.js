a386a7edc8bdbfe7f399f0045e4fb1b3c6465f1a
Konstantin Bantle <konstantin.bantle@cp-schikora.de>
2022-05-18 10:46:55 +0200

    finished VideoSettigns and moved to correct file

--- src/pages/soundSettings.js
+++ src/pages/soundSettings.js
@@ -1,73 +1,15 @@
 import * as React from 'react'
 import { Link } from 'gatsby'
-import {
-    arrow,
-    buttonWrapper,
-    defaultButton,
-    buttonText,
-    buttonIcon,
-    wrapperSoundBackground,
-    nonSliderPage,
-    switchWrapper
-} from '../components/layout.module.css'
-
-import idle from '../assets/idle.svg'
-import hdmi from '../assets/HDMI.svg'
-import clickshare from '../assets/Clickshare.svg'
-import arrowToGoBack from '../assets/BackArrow.svg'
-
-import Switch from '@mui/material/Switch';
 
 
-import { createTheme, ThemeProvider } from '@mui/material/styles';
 
-const label = { inputProps: { 'aria-label': 'Switch demo' } };
-
-
-const theme = createTheme({
-    status: {
-      danger: '#e53e3e',
-    },
-    palette: {
-      primary: {
-          main: '#000000',
-      }
-    },
-  });
-
-const soundSettingsPage = () => {
+const videoSettingsPage = () => {
     return(
-        <main className={wrapperSoundBackground}>
-            <title>SoundSettings</title>
-            <Link to="/"><img src = {arrowToGoBack} alt = "Load failed"  height="100" className={arrow}/></Link>
-            <div className={nonSliderPage}>
-                <div className={switchWrapper}>
-                    <ThemeProvider theme={theme}>
-                        <Switch {...label} color = "primary"/>
-
-                    </ThemeProvider>
-                </div>
-                <div className={buttonWrapper}> 
-                        <button className={defaultButton}>
-                            <img src = {idle} alt = "Load failed" width="150" height="150" className={buttonIcon}/>
-                            <span className={buttonText}>Idle Mode</span>
-                        </button>
-                        <button className={defaultButton}>
-                            <img src = {hdmi} alt = "Load failed" width="150" height="150" className={buttonIcon}/>
-                            <span className={buttonText}>HDMI 1</span>
-                        </button>                        
-                        <button className={defaultButton}>
-                            <img src = {hdmi} alt = "Load failed" width="150" height="150" className={buttonIcon}/>
-                            <span className={buttonText}>HDMI 2</span>
-                        </button>
-                        <button className={defaultButton}>
-                            <img src = {clickshare} alt = "Load failed" width="150" height="150" className={buttonIcon}/>
-                            <span className={buttonText}>Clickshare</span>
-                        </button>
-                    </div>
-                </div>
+        <main>
+            <title>VideoSettings</title>
+            <h1>video Settings</h1>
+            <Link to="/">Home</Link>
         </main>
     )
 }
-
-export default soundSettingsPage
+export default videoSettingsPage
--- src/pages/videoSettings.js
+++ src/pages/videoSettings.js
@@ -1,16 +1,86 @@
+
+
 import * as React from 'react'
 import { Link } from 'gatsby'
+import {
+    arrow,
+    buttonWrapper,
+    defaultButton,
+    buttonText,
+    buttonIcon,
+    wrapperSoundBackground,
+    nonSliderPage,
+    switchWrapper,
+    switchButton,
+    defaultText,
+    sliderIcon
+} from '../components/layout.module.css'
+
+import idle from '../assets/idle.svg'
+import hdmi from '../assets/HDMI.svg'
+import clickshare from '../assets/Clickshare.svg'
+import arrowToGoBack from '../assets/BackArrow.svg'
+import webexLogo from '../assets/Webex.svg'
+
+import Switch from '@mui/material/Switch';
 
 
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
-const videoSettingsPage = () => {
+const label = { inputProps: { 'aria-label': 'Switch demo' } };
+
+
+const theme = createTheme({
+    status: {
+      danger: '#e53e3e',
+    },
+    palette: {
+      primary: {
+          main: '#E20074',
+      }
+    },
+  });
+
+const soundSettingsPage = () => {
     return(
-        <main>
-            <title>VideoSettings</title>
-            <h1>video Settings</h1>
-            <Link to="/">Home</Link>
+        <main className={wrapperSoundBackground}>
+            <title>SoundSettings</title>
+            <Link to="/"><img src = {arrowToGoBack} alt = "Load failed"  height="100" className={arrow}/></Link>
+            <div className={nonSliderPage}>
+                <div className={switchWrapper}>
+                    <ThemeProvider theme={theme}>
+                        <div className={switchButton}>
+                            <Switch {...label} color = "primary"/>
+                        </div>
+
+                    </ThemeProvider>
+
+                    <p className={defaultText}>Webex</p>
+
+                    <img src = {webexLogo} alt = "Load failed"  height="120" className={sliderIcon}/>
+
+                </div>
+                <div className={buttonWrapper}> 
+                        <button className={defaultButton}>
+                            <img src = {idle} alt = "Load failed" width="150" height="150" className={buttonIcon}/>
+                            <span className={buttonText}>Idle Mode</span>
+                        </button>
+                        <button className={defaultButton}>
+                            <img src = {hdmi} alt = "Load failed" width="150" height="150" className={buttonIcon}/>
+                            <span className={buttonText}>HDMI 1</span>
+                        </button>                        
+                        <button className={defaultButton}>
+                            <img src = {hdmi} alt = "Load failed" width="150" height="150" className={buttonIcon}/>
+                            <span className={buttonText}>HDMI 2</span>
+                        </button>
+                        <button className={defaultButton}>
+                            <img src = {clickshare} alt = "Load failed" width="150" height="150" className={buttonIcon}/>
+                            <span className={buttonText}>Clickshare</span>
+                        </button>
+                    </div>
+                </div>
         </main>
     )
 }
 
-export default videoSettingsPage
+export default soundSettingsPage
