f96981632406b87910ddd9b06d192f20a2447e6a
Konstantin Bantle <konstantin.bantle@cp-schikora.de>
2022-05-18 12:20:52 +0200

    fixed Startpage Layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,15 +14,15 @@ import {
 
 const IndexPage = () => {
   return (
+    
     <main className={welcomeWrapper}>
+        <Link to='/lightSettings' className={welcomeLight}></Link>
+        <Link to='/videoSettings' className={welcomeVideo}></Link>
+        <Link to='/soundSettings' className={welcomeSound}></Link>
+        <Link to='/blindsControls' className={welcomeBlinds}></Link>
 
-      <Link to='/lightSettings' className={welcomeLight}></Link>
-      <Link to='/videoSettings' className={welcomeVideo}></Link>
-      <Link to='/soundSettings' className={welcomeSound}></Link>
-      <Link to='/blindsControls' className={welcomeBlinds}></Link>
-
-      <Link to='/' className={welcomeHelp}></Link>
-
+        <Link to='/' className={welcomeHelp}></Link>
+      
 
     </main>
   )
