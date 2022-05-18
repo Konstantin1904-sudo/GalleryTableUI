23f963fd7410c8e60d41aeca91df20fd55a0d99c
Konstantin Bantle <konstantin.bantle@cp-schikora.de>
2022-05-18 11:20:40 +0200

    added BlindsPage

--- src/pages/blindsControls.js
+++ src/pages/blindsControls.js
@@ -2,13 +2,43 @@ import * as React from 'react'
 import { Link } from 'gatsby'
 
 
+import {
+    arrow,
+    blindsWrapper,
+    defaultButton,
+    buttonIcon,
+    wrapperBlindsBackground,
+    nonSliderPage,
+    buttonWrapper
+
+} from '../components/layout.module.css'
+
+import arrowToGoBack from '../assets/BackArrow.svg'
+
+import arrowUp from '../assets/ArrowUp.svg'
+import minus from '../assets/Minus.svg'
+import arrowDown from '../assets/ArrowDown.svg'
+
+
 
 const blindControlPage = () => {
     return(
-        <main>
+        <main className={wrapperBlindsBackground}>
             <title>Blinds</title>
-            <h1>Blinds</h1>
-            <Link to="/">Home</Link>
+            <Link to="/"><img src = {arrowToGoBack} alt = "Load failed"  height="100" className={arrow}/></Link>
+                
+                <div className={blindsWrapper}> 
+                        <button className={defaultButton}>
+                            <img src = {arrowUp} alt = "Load failed" width="150" height="150" className={buttonIcon}/>
+                        </button>
+                        <button className={defaultButton}>
+                            <img src = {minus} alt = "Load failed" width="150" height="150" className={buttonIcon}/>
+                        </button>                        
+                        <button className={defaultButton}>
+                            <img src = {arrowDown} alt = "Load failed" width="150" height="150" className={buttonIcon}/>
+                        </button>
+
+                    </div>
         </main>
     )
 }
