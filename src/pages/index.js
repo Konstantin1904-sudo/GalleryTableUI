1346fc97f580bf2922f2111974f95ebcf54aa0af
Konstantin Bantle <konstantin.bantle@cp-schikora.de>
2022-05-18 11:50:20 +0200

    added Start Page

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,15 +2,27 @@ import * as React from 'react'
 import { Link } from 'gatsby'
 import {link} from '../components/layout.module.css'
 
+import {
+  welcomeWrapper,
+  welcomeLight,
+  welcomeVideo,
+  welcomeSound,
+  welcomeBlinds,
+  welcomeHelp
+} from '../components/layout.module.css'
+
 
 const IndexPage = () => {
   return (
-    <main>
+    <main className={welcomeWrapper}>
 
-      <Link to='/lightSettings' className={link}>Light settings</Link>
-      <Link to='/videoSettings'>Video settings</Link>
-      <Link to='/soundSettings'>Sound settings</Link>
-      <Link to='/blindsControls'>Blinds Controls</Link>
+      <Link to='/lightSettings' className={welcomeLight}></Link>
+      <Link to='/videoSettings' className={welcomeVideo}></Link>
+      <Link to='/soundSettings' className={welcomeSound}></Link>
+      <Link to='/blindsControls' className={welcomeBlinds}></Link>
+
+      <Link to='/' className={welcomeHelp}></Link>
+
 
     </main>
   )
